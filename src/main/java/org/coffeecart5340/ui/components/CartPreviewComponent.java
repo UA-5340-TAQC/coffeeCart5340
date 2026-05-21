d35aa8f1ff80af6980471e5356ec980519638360
BogdanBorsukevich <borsukevich.bogdan173@gmail.com>
2026-05-22 02:03:35 +0300

    Implement Cart-PreviewComponent

---
+++ src/main/java/org/coffeecart5340/ui/components/CartPreviewComponent.java
@@ -0,0 +1,40 @@
+package org.coffeecart5340.ui.components;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class CartPreviewComponent extends BaseComponent {
+
+    @FindBy(xpath = ".//span[1]")
+    private WebElement itemName;
+
+    @FindBy(xpath = ".//span[@class='unit-desc']")
+    private WebElement itemAmount;
+
+    @FindBy(xpath = ".//button[text()='+']")
+    private WebElement plusButton;
+
+    @FindBy(xpath = ".//button[text()='-']")
+    private WebElement minusButton;
+
+    public CartPreviewComponent(WebDriver driver, WebElement rootElement) {
+        super(driver, rootElement);
+    }
+
+    public String getItemName() {
+        return itemName.getText();
+    }
+
+    public String getItemAmount() {
+        return itemAmount.getText();
+    }
+
+    public void clickPlus() {
+        waitAndClickElement(plusButton);
+    }
+
+    public void clickMinus() {
+        waitAndClickElement(minusButton);
+    }
+}
--- src/main/java/org/coffeecart5340/ui/pages/MenuPage.java
+++ src/main/java/org/coffeecart5340/ui/pages/MenuPage.java
@@ -1,9 +1,26 @@
 package org.coffeecart5340.ui.pages;
 
+import org.coffeecart5340.ui.components.CartPreviewComponent;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
 
 public class MenuPage extends BasePage {
+
     public MenuPage(WebDriver driver) {
         super(driver);
+    }
+
+    @FindBy(css = "ul.cart-preview li.list-item")
+
+    private List<WebElement> cartPreviewElements;
+
+
+    public List<CartPreviewComponent> getCartPreviews() {
+        return cartPreviewElements.stream()
+    .map(element -> new CartPreviewComponent(driver, element))
+    .toList();
     }
 }
