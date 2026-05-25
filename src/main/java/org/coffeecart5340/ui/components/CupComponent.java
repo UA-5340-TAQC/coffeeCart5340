6330baaf0f5a67a4c0e678b08e2ace0ecc3c09ad
Liubomyr Halamaha <galamagal86@gmail.com>
2026-05-25 12:22:20 +0300

    fix

--- src/main/java/org/coffeecart5340/ui/components/CupComponent.java
+++ src/main/java/org/coffeecart5340/ui/components/CupComponent.java
@@ -1,21 +1,21 @@
 package org.coffeecart5340.ui.components;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class CupComponent extends BaseComponent {
-    private final By ingredientLocator = By.cssSelector("div");
+    @FindBy(xpath = ".//div[@class='cup-body']/div")
+    private List<WebElement> ingredients;
 
     public CupComponent(WebElement rootElement) {
         super(rootElement);
     }
 
     public List<IngredientComponent> getIngredients() {
-        List<WebElement> ingredientElements = rootElement.findElements(ingredientLocator);
-
-        return ingredientElements.stream()
+        return ingredients.stream()
                 .map(IngredientComponent::new)
                 .collect(Collectors.toList());
     }
