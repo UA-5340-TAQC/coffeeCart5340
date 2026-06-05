07b75ba6f938dcbffbd3d4449e5994d513be9b62
Liubomyr Halamaha <galamagal86@gmail.com>
2026-06-05 12:51:24 +0300

    added parallel cucumber test

--- src/test/java/org/coffeecart5340/cucumber/TestRunnerCucumber.java
+++ src/test/java/org/coffeecart5340/cucumber/TestRunnerCucumber.java
@@ -2,6 +2,8 @@ package org.coffeecart5340.cucumber;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "src/test/resources/features",
@@ -13,10 +15,20 @@ import io.cucumber.testng.CucumberOptions;
                 "pretty",
                 "html:target/cucumber-report.html",
                 "json:target/cucumber.json",
+                "summary",
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
         },
         monochrome = true
 )
 public class TestRunnerCucumber extends AbstractTestNGCucumberTests {
+        @BeforeClass
+        public void beforeClass() {
+                System.setProperty("allure.results.suite.name", "Cucumber BDD Tests");
+        }
 
+        @Override
+        @DataProvider(parallel = true)
+        public Object[][] scenarios() {
+                return super.scenarios();
+        }
 }
